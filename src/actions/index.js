52b25a2deb7abe2ba8feac7c41b28f0de842b49d
SimonGodefroid <simon.godefroid@gmail.com>
2017-11-16 11:47:38 +0100

    s05l57 added axios and added the action creator to fetch the weather data through axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,15 @@
+import axios from 'axios';
+
 const API_KEY = '45b52714817a2a57145a20af5407037e';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+// action creator for action FETCH_WEATHER
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},fr`;
+	const request = axios.get(url);
+	// payload is the data for the action
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	};
+}
