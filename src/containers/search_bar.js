a3f635007bf568e15c9327b54efe98c56e331611
SimonGodefroid <simon.godefroid@gmail.com>
2017-11-16 12:04:11 +0100

    s05l58 mapped the dispatch to props and connected the form submission with the action creator

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-
-export default class SearchBar extends Component {
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
+class SearchBar extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			term: ''
 		};
 		this.onInputChange = this.onInputChange.bind(this);
+		this.onFormSubmit = this.onFormSubmit.bind(this);
 	}
 	onInputChange(event) {
 		console.log(event.target.value);
@@ -15,6 +18,8 @@ export default class SearchBar extends Component {
 	onFormSubmit(event) {
 		event.preventDefault();
 		// We need to go an fetch weather data
+		this.props.fetchWeather(this.state.term);
+		this.setState({ term: '' });
 	}
 
 	render() {
@@ -35,3 +40,9 @@ export default class SearchBar extends Component {
 		);
 	}
 }
+
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
