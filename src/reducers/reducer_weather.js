3ce6c3eac672adbe7c41c2e7e584b621a446dfc6
SimonGodefroid <simon.godefroid@gmail.com>
2017-11-16 15:25:18 +0100

    implemented the reducer weather

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,5 +1,10 @@
+import FETCH_WEATHER from '../actions/index';
+
 // a reducer returns a piece of a state, it receives an action from the action creator
 export default function(state = null, action) {
-	console.log('WeatherReducer#Action received', action);
+	switch (action.type) {
+		case FETCH_WEATHER:
+			return [action.payload.data, ...state];
+	}
 	return state;
 }
